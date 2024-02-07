1acad0af30d9a09ff4bde322684865e7624db554
Filip Szuberski <filip.szuberski@gmail.com>
2024-02-07 23:44:53 +0000

    java/wc: process file line by line instead of loading it entirely into memory

--- java/wc/src/main/java/com/fszuberski/wc/Main.java
+++ java/wc/src/main/java/com/fszuberski/wc/Main.java
@@ -1,7 +1,20 @@
 package com.fszuberski.wc;
 
+import com.fszuberski.wc.adapter.ConsoleInputAdapter;
+import com.fszuberski.wc.application.port.in.CountUseCase;
+import com.fszuberski.wc.application.service.CountService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // simple di
+        final CountUseCase countUseCase = new CountService();
+        final ConsoleInputAdapter consoleInputAdapter = new ConsoleInputAdapter(countUseCase);
+
+        // run
+        try {
+            consoleInputAdapter.readInputAndCount(args);
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
     }
 }
